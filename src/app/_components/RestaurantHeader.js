974105bc549eb5b68cd7ab5dff9c3ecdb37836fa
singhxamit21 <singhxamit21@gmail.com>
2024-03-17 20:52:35 +0530

    initial commit of signUp page

--- src/app/_components/RestaurantHeader.js
+++ src/app/_components/RestaurantHeader.js
@@ -1,18 +1,47 @@
+'use client'
+import { useRouter,usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+
 const { default: Link } = require("next/link")
 
 const RestaurantHeader = () => {
-    return(
+    const [details, setDetails] = useState();
+
+    const router = useRouter();
+    const pathName = usePathname();
+
+    useEffect(() => {
+        let data = localStorage.getItem("restaurantUser")
+        if (!data && pathName == "/restaurant/dashboard") {
+            router.push("/restaurant")
+        } else if(data && pathName == "/restaurant"){
+            router.push("/restaurant/dashboard")
+        } else {
+            setDetails(JSON.parse(data))
+        }
+    },[])
+
+    const logout = () => {
+        localStorage.removeItem("restaurantUser")
+        router.push("/restaurant")
+    }
+    return (
         <div className="header-wrapper">
             <div className="logo">
-                <img style={{width:100}} src="https://img.freepik.com/free-vector/detailed-chef-logo-template_23-2148987940.jpg?w=740&t=st=1709566300~exp=1709566900~hmac=70665722ef838160946b80548dec3357362b03939aa9655d890a4202c781ee0a"/>
+                <img style={{ width: 100 }} src="https://img.freepik.com/free-vector/detailed-chef-logo-template_23-2148987940.jpg?w=740&t=st=1709566300~exp=1709566900~hmac=70665722ef838160946b80548dec3357362b03939aa9655d890a4202c781ee0a" />
             </div>
             <ul>
                 <li>
                     <Link href="/">Home</Link>
-                </li>
-                <li>
-                    <Link href="/">Login/SignUp</Link>
-                </li>
+                </li>    
+                    {
+                        details && details.name ?
+                        <>
+                        <li><button onClick={logout}>Logout</button></li>
+                        <li><Link href="/">Profile</Link></li>
+                        </>
+                        : <li><Link href="/">Login/SignUp</Link></li>
+                    }
                 <li>
                     <Link href="/">Profile</Link>
                 </li>
--- src/app/lib/restaurantsModel.js
+++ src/app/lib/restaurantsModel.js
@@ -2,7 +2,12 @@ const { default: mongoose } = require("mongoose");
 
 
 const restaurantModel = new mongoose.Schema({
-    name:String
+    email:String,
+    password:String,
+    name:String,
+    city:String,
+    address:String,
+    contact:String
 })
 
 export const restaurantSchema = mongoose.models.restaurants || mongoose.model("restaurants",restaurantModel)
