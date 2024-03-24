4769812d55b1af8d47ea8d257ce06453f5eb0a57
singhxamit21 <singhxamit21@gmail.com>
2024-03-24 14:20:56 +0530

    login page api integration initial commit

--- src/app/_components/RestaurantLogin.js
+++ src/app/_components/RestaurantLogin.js
@@ -1,16 +1,47 @@
+import { useState } from "react";
+import "../restaurant/style.css"
+
 const RestaurantLogin = () => {
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    const [error,setError] = useState(false)
+
+    const handleClick = async () => {
+        if(!email || !password){
+            setError(true)
+            return false;
+        }else{
+            setError(false)
+        }
+        console.log(email,password)
+        let response = await fetch("http://localhost:3000/api/restaurant", {
+            method: "POST",
+            body: JSON.stringify({ email,password,login:true })
+        })
+        response = await response.json();
+        console.log(response)
+        if (response.success) {
+            alert("Login Successfully")
+            // const { result } = response
+            // delete result.password;
+            // localStorage.setItem("restaurantUser", JSON.stringify(result));
+            // router.push("/restaurant/dashboard")
+        }
+    }
     return(
         <>
         <h3>Login Component</h3>
         <div>
             <div className="input-wrapper">
-                <input type="text" placeholder="enter email id" className="input-field"/>
+                <input type="text" placeholder="enter email id" value={email} onChange={(e)=> setEmail(e.target.value)} className="input-field"/>
+                {error && !email && <span className="input-error">Please enter valid email</span>}
             </div>
             <div className="input-wrapper">
-                <input type="password" placeholder="enter password" className="input-field"/>
+                <input type="password" placeholder="enter password" value={password} onChange={(e)=> setPassword(e.target.value)} className="input-field"/>
+                {error && !password && <span className="input-error">Please enter valid password</span>}
             </div>
             <div className="input-wrapper">
-                <button className="button">Login</button>
+                <button className="button" onClick={handleClick}>Login</button>
             </div>
         </div>
         </>
--- src/app/api/restaurant/route.js
+++ src/app/api/restaurant/route.js
@@ -4,17 +4,22 @@ import mongoose from "mongoose";
 import { NextResponse } from "next/server";
 
 
-export async function GET(){
-    await mongoose.connect(connectionStr,{})
+export async function GET() {
+    await mongoose.connect(connectionStr, {})
     const data = await restaurantSchema.find()
     console.log(data)
-    return NextResponse.json({result:data})
+    return NextResponse.json({ result: data })
 }
 
-export async function POST(request){
+export async function POST(request) {
     let payload = await request.json();
-    await mongoose.connect(connectionStr,{})
-    const restaurant = new restaurantSchema(payload)
-    const result = await restaurant.save()
-    return NextResponse.json({result,success:true})
+    await mongoose.connect(connectionStr, {});
+    let result;
+    if (payload.login) {
+        result = await restaurantSchema.findOne({ email: payload.email, password: payload.password })
+    } else {
+        const restaurant = new restaurantSchema(payload)
+        result = await restaurant.save()
+    }
+    return NextResponse.json({ result, success: true })
 }
